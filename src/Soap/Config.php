a315f2763ba5cf7880dea5523f95c233e8eb18d7
Ewersonfc <ewersonfc@gmail.com>
2023-01-27 17:07:44 -0300

    identação
    identação

--- src/Soap/Config.php
+++ src/Soap/Config.php
@@ -8,7 +8,7 @@ class Config
 
 	const WSDL_HM = 'https://cobranca.homologa.bb.com.br:7101/registrarBoleto';
 
-    const WSDL_PRODUCTION = 'https://cobranca.bb.com.br:7101/registrarBoleto';
+    	const WSDL_PRODUCTION = 'https://cobranca.bb.com.br:7101/registrarBoleto';
 
 	const NAMESPACE = 'http://www.tibco.com/schemas/bws_registro_cbr/Recursos/XSD/Schema.xsd';
 
